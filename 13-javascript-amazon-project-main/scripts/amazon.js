3cc3e70f35f401cfadf6b1b27efd1d604a7d2da1
Sirisha <sirisha.goutsystems@gmail.com>
2023-12-29 18:52:45 -0500

    import cart and product from cart.js and products.js

--- 13-javascript-amazon-project-main/scripts/amazon.js
+++ 13-javascript-amazon-project-main/scripts/amazon.js
@@ -1,3 +1,6 @@
+import {cart} from '../data/cart.js';
+import {products} from '../data/products.js';
+
 let productHTML = '';
 products.forEach((product) => {
 
@@ -41,7 +44,7 @@ products.forEach((product) => {
 
     <div class="product-spacer"></div>
 
-    <div class="added-to-cart">
+    <div class="added-to-cart js-added-to-cart-${product.id}">
       <img src="images/icons/checkmark.png">
       Added
     </div>
@@ -58,9 +61,10 @@ document.querySelector('.js-products-grid').innerHTML = productHTML;
 
 document.querySelectorAll('.js-add-to-cart').
 forEach((button) => {
-    button.addEventListener('click', () => {
-       const productId = button.dataset.productId;
 
+  let addedMessageTimeoutId;
+    button.addEventListener('click', () => {
+       const {productId}= button.dataset;
        let matchingItem;
        cart.forEach((item) => {
         if(productId === item.productId) {
@@ -79,9 +83,8 @@ forEach((button) => {
         matchingItem.quantity += quantity;
        } else {
         cart.push({
-            productId:productId,
-            quantity : quantity
-
+            productId,
+            quantity
        });
        }
        let cartQuantity = 0;
@@ -90,7 +93,25 @@ forEach((button) => {
        });
       
        document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
+      const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
+
+      addedMessage.classList.add('added-to-cart-visible');
        
+       setTimeout(() => {
+        if(addedMessageTimeoutId) {
+          clearTimeout(addedMessageTimeoutId);
+        }
+       
+     const timeoutId = setTimeout(() => {
+        addedMessage.classList.remove('added-to-cart-visible');
+      
+      },2000);
+
+      addedMessageTimeoutId = timeoutId;
+    });
+
+      
      
     });
 });
