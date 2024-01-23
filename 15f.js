3e9f3e7e22797be871a02cd203be536b225f9994
Sirisha <sirisha.goutsystems@gmail.com>
2024-01-23 12:51:10 -0500

    Exercise15a-15g

---
+++ 15f.js
@@ -0,0 +1,8 @@
+
+export function isWeekend(date){
+    const dayOfWeek = date.format('dddd');
+    return  dayOfWeek === 'Saturday' || dayOfWeek === 'Sunday' ;
+    
+  }
+
+  export default isWeekend;
